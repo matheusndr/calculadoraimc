703e846d49a1aa01d7827b33abf264e331765ba4
matheusandrade <matheusandrade@outlook.de>
2024-12-26 16:38:22 +0000

    mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,13 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Recuperar os compoentes EditText
-        // Criar uma variavel e associar o componente de UI<EditText>
-        // Recuperar o botão da tela
-
-        // Colocar acao no botao setOnClickListener
-        // Recuperar o texto digitado no edt peso
-
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
 
@@ -26,28 +20,37 @@ class MainActivity : AppCompatActivity() {
         btnCalcular.setOnClickListener {
 
 
-            val pesoStr: String = edtPeso.text. toString()
+            val pesoStr: String = edtPeso.text.toString()
             val alturaStr: String = edtAltura.text.toString()
 
-            if(pesoStr == "" || alturaStr == ""){
-                // Mostrar mensagem para o usuario
+            if (pesoStr == "" || alturaStr == "") {
                 Snackbar
                     .make(
-                    edtPeso,
-                    "Preencha todos os campos",
-                    Snackbar.LENGTH_LONG
-                )
+                        edtPeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
                     .show()
 
             } else {
                 val peso = pesoStr.toFloat()
                 val altura = alturaStr.toFloat()
 
-            val alturaQ2 = altura + altura
-            val resultado = peso / alturaQ2
+                val alturaQ2 = altura + altura
+                val resultado = peso / alturaQ2
 
-            println("Roque acao do botao" + resultado)
+                // Navegar para proxima tela
+                // Criar o layout da proxima tela
+                // Passar dados (resultado) para proxima tela
+
+                // Intent -  Classe do proprio android
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+                println("Roque acao do botao" + resultado)
+            }
         }
     }
-}
 }
